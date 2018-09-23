4a92c01d03a6cc483ac4cedf54c5191ca533aad7
lcyyong <43041462+lcyyong@users.noreply.github.com>
2018-09-24 00:02:16 +0800

    change header

--- rysayido/src/main/webapp/js/common.js
+++ rysayido/src/main/webapp/js/common.js
@@ -139,7 +139,7 @@
 		$('#galleryDiv').html("");
 		var content = "";
 		for (i=0;i<imgSrc.length;i++){
-				content+='<div class="col-lg-2 col-md-3 col-xs-4">';
+				content+='<div class="col-lg-3 col-md-4 col-xs-6">';
 				content+='		<a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="" data-image="'+imgSrc[i]+'" data-target="#image-gallery">';
 				content+='			<img class="img-thumbnail" src="'+imgSrc[i]+'" alt="Another alt text" onContextMenu="return false;">';
 				content+='		</a>';
